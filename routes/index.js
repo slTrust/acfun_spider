c5de68d3330274694907c3b21959d3745a67213d
slTrust <trustfor@sina.cn>
2020-04-15 11:39:11 +0800

    修改 https => http

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/spiderProtocol', (req, res) => {
     },
     config: {
       contentList: {
-        url: 'https://localhost:3000/content',
+        url: 'http://localhost:3000/content',
         pageSizeLimit: 20,
         frequencyLimit: 5,
       },
